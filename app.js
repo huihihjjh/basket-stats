a6d1a0360c14d4b19af051855b62eaf5d89b64ea
huihihjjh <huihihjjh@gmail.com>
2026-01-01 19:30:55 -0300

    Actualizar app.js

--- app.js
+++ app.js
@@ -1,17 +1,1 @@
-alert("JS cargó");
-
-if (typeof firebase === "undefined") {
-  alert("Firebase NO cargó");
-} else {
-  alert("Firebase SÍ cargó");
-}
-
-try {
-  db.collection("prueba").add({
-    ok: true,
-    fecha: Date.now()
-  });
-  alert("Intenté escribir en Firestore");
-} catch (e) {
-  alert("Error: " + e.message);
-}
+alert("app.js cargó");
