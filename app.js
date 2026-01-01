e37b92669ba178708858a5298fb7c470d4df7e75
huihihjjh <huihihjjh@gmail.com>
2026-01-01 19:23:27 -0300

    Actualizar app.js

--- app.js
+++ app.js
@@ -1,33 +1,4 @@
-const input = document.getElementById("nombre");
-const boton = document.getElementById("agregar");
-const lista = document.getElementById("lista");
-
-boton.addEventListener("click", () => {
-  const nombre = input.value.trim();
-  if (nombre === "") return;
-
-  db.collection("jugadores").add({
-    nombre: nombre,
-    puntos: 0,
-    triples: 0,
-    dobles: 0,
-    asistencias: 0,
-    rebotes: 0,
-    tapones: 0,
-    creado: Date.now()
-  });
-
-  input.value = "";
+db.collection("prueba").add({
+  ok: true,
+  fecha: Date.now()
 });
-
-db.collection("jugadores")
-  .orderBy("creado")
-  .onSnapshot(snapshot => {
-    lista.innerHTML = "";
-    snapshot.forEach(doc => {
-      const j = doc.data();
-      const li = document.createElement("li");
-      li.textContent = `${j.nombre} - ${j.puntos} pts`;
-      lista.appendChild(li);
-    });
-  });
