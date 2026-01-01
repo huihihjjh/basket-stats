138bbf291f9cf5ebc9625b7163de10af941e1a01
huihihjjh <huihihjjh@gmail.com>
2026-01-01 19:28:31 -0300

    Actualizar app.js

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
-db.collection("prueba").add({
-  ok: true,
-  fecha: Date.now()
-});
+alert("JS cargó");
+
+if (typeof firebase === "undefined") {
+  alert("Firebase NO cargó");
+} else {
+  alert("Firebase SÍ cargó");
+}
+
+try {
+  db.collection("prueba").add({
+    ok: true,
+    fecha: Date.now()
+  });
+  alert("Intenté escribir en Firestore");
+} catch (e) {
+  alert("Error: " + e.message);
+}
